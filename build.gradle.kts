4f6289d051a93c4a195e3287fa2e35ace6e18ffc
RednedEpic <redned235@gmail.com>
2024-12-11 17:25:21 +0000

    Add missing versions and loaders

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,10 @@ plugins {
     id("com.modrinth.minotaur") version "2.+"
 }
 
-val supportedVersions = listOf("1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6", "1.21")
+val supportedVersions = listOf(
+    "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6",
+    "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4"
+)
 
 group = "me.redned"
 version = "1.0.0"
@@ -48,5 +51,6 @@ modrinth {
     versionType.set(if (snapshot) "beta" else "release")
     changelog.set(System.getenv("CHANGELOG") ?: "")
     uploadFile.set(tasks.jar)
+    loaders.set(listOf("paper", "spigot"))
     gameVersions.set(supportedVersions)
 }
